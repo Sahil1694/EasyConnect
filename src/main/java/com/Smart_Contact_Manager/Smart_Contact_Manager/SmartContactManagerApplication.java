22782fd9c842431e35e6297867d9db63beab21cb
Sahil Khilari <sahilkhilari16@gmail.com>
2024-08-15 11:08:40 +0530

    Initial commit

---
+++ src/main/java/com/Smart_Contact_Manager/Smart_Contact_Manager/SmartContactManagerApplication.java
@@ -0,0 +1,13 @@
+package com.Smart_Contact_Manager.Smart_Contact_Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartContactManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartContactManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Smart_Contact_Manager/Smart_Contact_Manager/SmartContactManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Smart_Contact_Manager.Smart_Contact_Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartContactManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
